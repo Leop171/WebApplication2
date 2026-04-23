bab70690e1fd7f49678122468f98baa6efed3659
Leop171 <141784647+Leop171@users.noreply.github.com>
2026-04-23 14:05:41 -0600

    Carpta contents
    Se sube carpeta contents actualizada con los nuevos diseños y se agrega carpeta AppStart enurtamiento para la encuestas

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Encuesta", action = "Registrar", id = UrlParameter.Optional }
+            );
+            */
+
+            routes.MapRoute(
+            name: "Encuesta",
+            url: "Encuesta/Responder/{enlace}",
+            defaults: new { controller = "Encuesta", action = "Responder" }
+            );
+
+            routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Inicio", action = "Login", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
